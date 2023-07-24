30f74e28e4b62bec21c6f49eb39bcec06b4b9da5
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-07-24 14:17:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Sigismund:l10NKyvL5j5Ne6RU@db.oq1uajs.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
