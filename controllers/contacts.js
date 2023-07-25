dd31396bb081710222434d7b54d1b197b71dee35
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-07-25 22:54:43 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,9 +5,8 @@ const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, { skip, limit }).populate(
-    "owner",
-    "name"
+  const result = await Contact.find({ owner }, "", { page, limit }).populate(
+    "owner"
   );
   res.json(result);
 };
